4239efbb046a85e63729fc6de458e3ac4c497d55
Yelyzaveta Domasheva <lizidomasheva@gmail.com>
2020-04-13 15:04:29 +0300

    hw-12

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import PNotify from 'pnotify/dist/es/PNotify.js';
 import 'pnotify/dist/PNotifyBrightTheme.css';
 
 
-
-// const refs = {
-//   input: document.querySelector('.search__input'),
-//   div: document.querySelector('.country__description'),
-// };
-
 refs.input.addEventListener('input', debounce(handleInput, 500));
 
 function handleInput(e) {
@@ -46,43 +40,3 @@ function handleInput(e) {
       renderCountry(data)}
   }).catch(error => console.error('ERROR', error));
 }
-
-// function makeFetch(name) {
-//   fetch(`${baseURL}/name/${name}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     if(data.length>10) {
-//       PNotify.error({
-//         title: 'Too many matches founds',
-//         text: 'Please enter a more specific query!'
-//       });
-//     } else if (data.length>1&&data.length<=10) {
-//       otherRender(data)
-//     } else {
-
-//     renderCountry(data)}
-//   }).catch(error => console.error('ERROR', error));
-
-// }
-
-
-  // function renderCountry (countries) {
-  //   const markup = countries.map(country => countryTemplate(country)).join('');
-  //   refs.div.insertAdjacentHTML('beforeend', markup);
-
-  //   showCountry(markup)
-  // }
-
-  // function otherRender (countries) {
-  //   const markup = countries.map(country => secondTempl(country)).join('');
-  //   refs.div.insertAdjacentHTML('beforeend', markup);
-  //   showCountry(markup)
-  // }
-
-
-  // function showCountry (template) {
-  //   refs.div.innerHTML = template;
-  // }
-
-
